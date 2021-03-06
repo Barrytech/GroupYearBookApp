666a71cce87d95217f9e12a651936764c3fde0f4
Barrytech <barryabdoul2024@gmail.com>
2021-03-05 21:35:34 -0500

    added activities for all

---
+++ app/src/main/java/com/example/groupyearbookapp/ChrisActivity.java
@@ -0,0 +1,14 @@
+package com.example.groupyearbookapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChrisActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chris);
+    }
+}
---
+++ app/src/main/java/com/example/groupyearbookapp/HusnainActivity.java
@@ -0,0 +1,14 @@
+package com.example.groupyearbookapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HusnainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_husnain);
+    }
+}
